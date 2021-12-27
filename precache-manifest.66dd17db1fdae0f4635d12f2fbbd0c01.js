8ddc35135a41e543fab2f8aac27c305af55b280c
Fenomen-Alex <alexfenomen@gmail.com>
2021-12-27 20:49:10 +0200

    Updates

---
+++ precache-manifest.66dd17db1fdae0f4635d12f2fbbd0c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d099e9fb0ee2a1e157bde1ed49e2a752",
+    "url": "/Timer-app/index.html"
+  },
+  {
+    "revision": "9f53f6e3be8cc80070f9",
+    "url": "/Timer-app/static/css/main.d2ce90d3.chunk.css"
+  },
+  {
+    "revision": "3d624f49dffc2dad0eee",
+    "url": "/Timer-app/static/js/2.85743ade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Timer-app/static/js/2.85743ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f53f6e3be8cc80070f9",
+    "url": "/Timer-app/static/js/main.7289ad96.chunk.js"
+  },
+  {
+    "revision": "8089127227c75df2f82f",
+    "url": "/Timer-app/static/js/runtime-main.1d3cf59e.js"
+  }
+]);
